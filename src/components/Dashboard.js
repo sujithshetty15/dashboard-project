012b104647b92df72cc2eb955a40178f2111ffe9
sujith <sujith.20cs095@sode-edu.in>
2024-08-22 12:02:13 +0530

    change

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -92,7 +92,7 @@ const Dashboard = () => {
     })}
   </Grid>
 
-  {/* Add Category Dialog */}
+  {/*Category */}
   <Dialog open={openCategoryDialog} onClose={() => setOpenCategoryDialog(false)}>
     <DialogTitle>Add New Category</DialogTitle>
     <DialogContent>
@@ -116,7 +116,7 @@ const Dashboard = () => {
     </DialogActions>
   </Dialog>
 
-  {/* Add Widget Dialog */}
+  {/* Widget */}
   <Dialog open={openWidgetDialog} onClose={() => setOpenWidgetDialog(false)}>
     <DialogTitle>Add New Widget</DialogTitle>
     <DialogContent>
